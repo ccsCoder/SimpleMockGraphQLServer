c715b4bf97ae0fb51266a5d61011fd920e3da9b7
nishangu <nishangu@amazon.com>
2019-11-05 09:38:25 +0530

    Changing data

--- src/Types.js
+++ src/Types.js
@@ -13,6 +13,7 @@ const typeDefs = gql`
     }
     # This type specifies the entry points into our API. In this case
     # there is only one - "channels" - which returns a list of channels.
+  
     type Query {
       channels: [Channel]    # "[]" means this is a list of channels
       channel(id: ID!): Channel
