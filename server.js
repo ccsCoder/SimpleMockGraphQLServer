231d0e2257e5d6c9d1beecc3f523c99ab7617923
nishangu <nishangu@amazon.com>
2019-11-05 09:25:45 +0530

    Updated dependencies from apollo-server-1.4 to apollo-server-2.x. Updated code for 2.x

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
-// APOLLO provides implementations for different servers such as Koa, hapi, Express and restify. We are going to use Express.
-
 import express from 'express';
 import cors from 'cors';
-import {
-  graphqlExpress,
-  graphiqlExpress,
-} from 'graphql-server-express';
-import bodyParser from 'body-parser';
-import { PubSub } from 'graphql-server-express';
 
 import { schema } from './src/schema';
 
 const PORT = 7800;
-const server = express();
-server.use('*', cors({ origin: 'http://localhost:7800' }));
+const app = express();
+app.use('*', cors({ origin: 'http://localhost:7800' }));
 
-// ? Setup express middleware for graphql.
-// ? For every request to ‘/graphql’, Apollo will run through the entire 
-// ? query/mutation processing chain and return the result depending on the query/mutation from the client.
-server.use('/graphql', bodyParser.json(), graphqlExpress({
-  schema
-}));
+schema.applyMiddleware({
+    app,
+});
 
-// ? Test bed.
-server.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql'
-}));
-
-server.listen(PORT, () =>
-  console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
+app.listen(PORT, () =>
+  console.log(`GraphQL app is now running on http://localhost:${PORT}`)
 );
+
+export default app;
